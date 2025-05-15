d604dedc669887a695b8a27222b103a5008ddc68
Raghuttama K G <raghuttama01@gmail.com>
2025-05-15 20:19:24 +0530

    Just starting the Navbar part will somehow complete the 90% of the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React,{useEffect,useRef,useState,} from "react";
+import { cn } from "../utils";
+
+const Navbar=()=>{
+    const [isMenuOpen,setIsMenuOpen] = useState(false);
+     const router=useNa
+    
+    return(
+        <>
+         <div className="sticky top-0 z-[4]">
+            <button>
+                <span
+            </button>
+         </div>
+        </>
+    )
+}
+export default Navbar;
