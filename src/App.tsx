b2d75496d5a225e749cb0272d240f86f446b7af4
Raghuttama K G <raghuttama01@gmail.com>
2025-05-16 18:05:37 +0530

    Added the footer and data for use

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import CustumCursor from './components/CustumCursor';
 import Navbar from './components/Navbar';
 import ParticleBackground from './components/particleDrop';
 import { BrowserRouter } from 'react-router-dom';
-
+import ScrollProgressIndicator from './components/SCrollProgress';
+import Button from './components/Button';
 function App() {
   // Remove unused count state if not needed
   // const [count, setCount] = useState(0);
@@ -17,16 +18,23 @@ function App() {
       <Preloader />
       <CustumCursor />
       <ParticleBackground />
+      <ScrollProgressIndicator/>
       <BrowserRouter>
       <Navbar />
       </BrowserRouter>
       {/* Main Content */}
       <main className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Raghu Kumar Rao's Portfolio</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome to Raghuttama's  Portfolio</h1>
         <p className="text-lg text-gray-600 mb-8">
           Explore my projects, experience, and more!
+          <BrowserRouter>
+          <Button variant='secondary'>Learn More</Button ></BrowserRouter>
+          
+
+          
         </p>
         {/* Add more sections/components here as needed */}
+        
       </main>
     </div>
   );
